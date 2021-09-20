00f737876afcc7f52e14912630e159ffdd5262e3
Timothy Attah <timothyattah733@gmail.com>
2021-09-20 20:27:21 +0100

    Added new style to top and bottom div in App.jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { MainContainer } from './styles/Styles';
 import { images } from './components/images';
@@ -8,12 +8,35 @@ export const MainContent = styled.div`
 width: 75%;
 /* background: url(${images.BackgroundImage}); */
 background: linear-gradient(to bottom, rgba(0,0,0,0), #020230), url(${images.BackgroundImage});
+display: flex;
+flex-direction: column;
 `;
 export const Pyramid = styled.div`
 width: 25%;
 display: flex;
 align-items: center;
 justify-content: center;
+`;
+export const MainContentTop = styled.div`
+height: 50%;
+position: relative;
+`;
+export const MainContentBottom = styled.div`
+height: 50%;
+`;
+export const Timer = styled.div`
+width: 70px;
+height: 70px;
+border-radius: 50%;
+border: 5px solid var(--bg-white);
+display: flex;
+align-items: center;
+justify-content: center;
+font-size: 30px;
+font-weight: 700;
+position: absolute;
+bottom: 10px;
+left: 80px;
 `;
 
 const MoneyList = styled.ul`
@@ -40,13 +63,22 @@ li{
 `;
 
 export const App = () => {
+  const [ questionNumber ] = useState( 1 );
+  // const [ questionNumber, setQuestionNumber ] = useState( 1 );
   return (
     <MainContainer>
-      <MainContent>Moan</MainContent>
+      <MainContent>
+        <MainContentTop>
+          <Timer>30</Timer>
+        </MainContentTop>
+        <MainContentBottom>
+          questions and answers
+        </MainContentBottom>
+      </MainContent>
       <Pyramid>
         <MoneyList>
           { moneyPyramid.map( money => (
-            <li className="moneyListItem active">
+            <li className={questionNumber === money.id ? 'active' : ''}>
               <span className="number">{ money.id }</span>
               <span className="amount">{money.amount}</span>
             </li>
