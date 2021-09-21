2dd433f8c0662ea747f60435f6568a305007f624
Timothy Attah <timothyattah733@gmail.com>
2021-09-21 09:24:36 +0100

    Added moneyPyramid list in App.jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import styled from 'styled-components';
 import { MainContainer } from './styles/Styles';
 import { images } from './components/images';
-import { moneyPyramid, Data } from './components/Helper';
+import { Data } from './components/Helper';
 import { Quiz } from './components/questions/Quiz';
 
 export const MainContent = styled.div`
@@ -72,6 +72,26 @@ li{
 `;
 
 export const App = () => {
+  const moneyPyramid = useMemo(
+    () => [
+      { id: 1, amount: '$ 100' },
+      { id: 2, amount: '$ 200' },
+      { id: 3, amount: '$ 300' },
+      { id: 4, amount: '$ 500' },
+      { id: 5, amount: '$ 1000' },
+      { id: 6, amount: '$ 2000' },
+      { id: 7, amount: '$ 4000' },
+      { id: 8, amount: '$ 8000' },
+      { id: 9, amount: '$ 16000' },
+      { id: 10, amount: '$ 32000' },
+      { id: 11, amount: '$ 64000' },
+      { id: 12, amount: '$ 125000' },
+      { id: 13, amount: '$ 250000' },
+      { id: 14, amount: '$ 500000' },
+      { id: 15, amount: '$ 1000000' },
+    ].reverse(),
+    []
+  );
   const [ questionNumber, setQuestionNumber ] = useState( 1 );
   const [stop, setStop ] = useState( false );
   const [ earned, setEarned ] = useState( '$ 0' );
